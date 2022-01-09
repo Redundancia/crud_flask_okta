528ae47b51ef1efa5320b69af426a6acc4289611
Redundancia <hunyadiwalter94@gmail.com>
2022-01-09 13:34:48 +0100

    Add layout and index pages

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask.templating import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, world!"
+    """
+    Render the homepage.
+    """
+    posts_final = []
+    return render_template("blog/index.html", posts=posts_final)
